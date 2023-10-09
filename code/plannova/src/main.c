f476b29a92ec60281704c1efad9257b4f9b3059a
Brooke West <brookewest@ku.edu>
2023-10-09 17:04:41 -0500

    folders

---
+++ code/plannova/src/main.c
@@ -0,0 +1,7 @@
+int main() {
+    fork();
+    fork();
+    fork();
+
+    printf("Reached");
+}
---
+++ code/plannova/src/modules/MonthCal.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./MonthCal.css";
+
+function tileContent({}) {}
+
+function MonthCal() {
+  const [date, setDate] = useState<any>(new Date());
+
+  return (
+    <div>
+      <Calendar
+        onChange={setDate}
+        value={date}
+        defaultValue="month"
+        calendarType="US"
+        showNavigation={false}
+        prev2Label={null}
+        prevLabel={null}
+        nextLabel={null}
+        next2Label={null}
+      />
+    </div>
+  );
+}
+
+export default MonthCal;
