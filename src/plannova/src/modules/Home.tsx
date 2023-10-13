08b40bd01d320a3a185b8bb619c78143816b71b0
Brooke West <brookewest@ku.edu>
2023-10-13 15:23:39 -0500

    cleaned up homepage a bit

--- src/plannova/src/modules/Home.tsx
+++ src/plannova/src/modules/Home.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./Home.css";
 import MonthCal from "./MonthCal";
+import { CiCircleList, CiCalendar, CiPen } from "react-icons/ci";
 
 
 function Home(){
+
+  const currentDate = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+  const formattedDate = currentDate.toLocaleDateString(undefined, options);
+
     return(
-        <div className="home-page">
+        <div className="home-page" >
         <div>
           <div className="logo"> ★ PlanNova</div>
           <div className="week">weekly schedule</div>
-          <div className="year">current date </div>
           <div className="calendar">
+            <div className="date-title">{formattedDate}</div>
             <MonthCal />
           </div>
         </div>
@@ -22,11 +32,12 @@ function Home(){
             <Link to="/fitness-tracker" className="links">fitness tracker</Link>
             <button className="buttons button"> logout</button>
           </div>
-          <div className="daily-calendar">daily calendar</div>
+          <div className="daily-calendar"><CiCalendar className="icons"/>today's schedule</div>
           <input className="firstText"type='text' />
-          <div className="pinned-list">pinned lists</div>
+          <div className="pinned-list"> <Link to="/list" ><CiCircleList className="icons"/></Link>pinned lists
+          </div>
           <input className="secondText"type='text' />
-          <div className="notes">notes</div>
+          <div className="notes"><CiPen className="icons"/>notes</div>
           <input className="thirdText"type='text' />
         </div>
       </div>
