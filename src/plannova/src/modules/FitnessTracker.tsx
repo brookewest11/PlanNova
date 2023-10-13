f6cd7618f08fb81a5b6b430c862ccc91807ead97
Brooke West <brookewest@ku.edu>
2023-10-13 12:56:29 -0500

    added meal planner page

--- src/plannova/src/modules/FitnessTracker.tsx
+++ src/plannova/src/modules/FitnessTracker.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import "./FitnessTracker.css";
+import { Link } from 'react-router-dom';
 
 function FitnessTracker(){
     return(
-        <><h1 className='logo'> ★ PlanNova</h1><div>
-            <div>fitness tracker coming soon</div>
-        </div></>
+        <div className="grid-container">
+        <div className="grid-item">
+          <h1 className='logo'> ★ PlanNova</h1>
+        </div>
+        <div className="grid-item">
+          <div className="nav-buttons">
+            <Link to="/list" className="links">lists</Link>
+            <Link to="/meal-planning" className="links">meal planning</Link>
+            <Link to="/fitness-tracker" className="links">fitness tracker</Link>
+            <Link to="/" className="links">home</Link>
+            <button className="buttons button"> logout</button>
+          </div>
+        </div>
+      </div>
     )
 
 }
--- src/plannova/src/modules/List.tsx
+++ src/plannova/src/modules/List.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import "./List.css";
+import { Link } from 'react-router-dom';
 
 function List(){
     return(
-        <><h1 className='logo'> ★ PlanNova</h1><div>
-            list coming soon!
-        </div></>
+        <div className="grid-container">
+      <div className="grid-item">
+        <h1 className='logo'> ★ PlanNova</h1>
+      </div>
+      <div className="grid-item">
+      <div className="nav-buttons">
+            <Link to="/list" className="links">lists</Link>
+            <Link to="/meal-planning" className="links">meal planning</Link>
+            <Link to="/fitness-tracker" className="links">fitness tracker</Link>
+            <Link to="/" className="links">home</Link>
+            <button className="buttons button"> logout</button>
+          </div>
+      </div>
+    </div>
     )
 
 }
