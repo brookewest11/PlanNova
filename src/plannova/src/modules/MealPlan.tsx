c596eae993ce046200c22f7191a8c268b620479d
brookewest11 <brookewest@ku.edu>
2024-02-12 18:41:16 -0600

    messing with box order

--- src/plannova/src/modules/MealPlan.tsx
+++ src/plannova/src/modules/MealPlan.tsx
@@ -61,283 +61,285 @@ function MealPlan() {  //creates MealPlan function
         {/* heading and creates 7 meal boxes (each meal box has 3 input text boxes that change as you type in them and hold their value) */}
         <h2 className="title">yourname's meals </h2>
       </div>
-      <div className="grid-container2">
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            monday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[0]}
-                  onChange={(e) => handleInputChange(e, 0)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[1]}
-                  onChange={(e) => handleInputChange(e, 1)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[2]}
-                  onChange={(e) => handleInputChange(e, 2)}
-                  placeholder="add meal..."
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            tuesday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[3]}
-                  onChange={(e) => handleInputChange(e, 3)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[4]}
-                  onChange={(e) => handleInputChange(e, 4)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[5]}
-                  onChange={(e) => handleInputChange(e, 5)}
-                  placeholder="add meal..."
-                />
+      <div className="meal-grid-container2">
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              monday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[0]}
+                    onChange={(e) => handleInputChange(e, 0)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[1]}
+                    onChange={(e) => handleInputChange(e, 1)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[2]}
+                    onChange={(e) => handleInputChange(e, 2)}
+                    placeholder="add meal..."
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            wednesday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[6]}
-                  onChange={(e) => handleInputChange(e, 6)}
-                  placeholder="add meal..."
-                />
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              tuesday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[3]}
+                    onChange={(e) => handleInputChange(e, 3)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[4]}
+                    onChange={(e) => handleInputChange(e, 4)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[5]}
+                    onChange={(e) => handleInputChange(e, 5)}
+                    placeholder="add meal..."
+                  />
+                </div>
               </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[7]}
-                  onChange={(e) => handleInputChange(e, 7)}
-                  placeholder="add meal..."
-                />
+            </div>
+        
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              wednesday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[6]}
+                    onChange={(e) => handleInputChange(e, 6)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[7]}
+                    onChange={(e) => handleInputChange(e, 7)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[8]}
+                    onChange={(e) => handleInputChange(e, 8)}
+                    placeholder="add meal..."
+                  />
+                </div>
               </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[8]}
-                  onChange={(e) => handleInputChange(e, 8)}
-                  placeholder="add meal..."
-                />
               </div>
             </div>
           </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            thursday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[9]}
-                  onChange={(e) => handleInputChange(e, 9)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[10]}
-                  onChange={(e) => handleInputChange(e, 10)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[11]}
-                  onChange={(e) => handleInputChange(e, 11)}
-                  placeholder="add meal..."
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            friday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[12]}
-                  onChange={(e) => handleInputChange(e, 12)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[13]}
-                  onChange={(e) => handleInputChange(e, 13)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[14]}
-                  onChange={(e) => handleInputChange(e, 14)}
-                  placeholder="add meal..."
-                />
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              thursday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[9]}
+                    onChange={(e) => handleInputChange(e, 9)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[10]}
+                    onChange={(e) => handleInputChange(e, 10)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[11]}
+                    onChange={(e) => handleInputChange(e, 11)}
+                    placeholder="add meal..."
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            saturday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[15]}
-                  onChange={(e) => handleInputChange(e, 15)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[16]}
-                  onChange={(e) => handleInputChange(e, 16)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[17]}
-                  onChange={(e) => handleInputChange(e, 17)}
-                  placeholder="add meal..."
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item2">
-          <div className="mealbox">
-            {" "}
-            sunday
-            <div>
-              <br></br>
-              breakfast
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[18]}
-                  onChange={(e) => handleInputChange(e, 18)}
-                  placeholder="add meal..."
-                />
-              </div>
-              lunch
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[19]}
-                  onChange={(e) => handleInputChange(e, 19)}
-                  placeholder="add meal..."
-                />
-              </div>
-              dinner
-              <div key={0}>
-                <input
-                  className="input"
-                  type="text"
-                  value={inputValues[20]}
-                  onChange={(e) => handleInputChange(e, 20)}
-                  placeholder="add meal..."
-                />
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              friday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[12]}
+                    onChange={(e) => handleInputChange(e, 12)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[13]}
+                    onChange={(e) => handleInputChange(e, 13)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[14]}
+                    onChange={(e) => handleInputChange(e, 14)}
+                    placeholder="add meal..."
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              saturday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[15]}
+                    onChange={(e) => handleInputChange(e, 15)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[16]}
+                    onChange={(e) => handleInputChange(e, 16)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[17]}
+                    onChange={(e) => handleInputChange(e, 17)}
+                    placeholder="add meal..."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid-item2">
+            <div className="mealbox">
+              {" "}
+              sunday
+              <div>
+                <br></br>
+                breakfast
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[18]}
+                    onChange={(e) => handleInputChange(e, 18)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                lunch
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[19]}
+                    onChange={(e) => handleInputChange(e, 19)}
+                    placeholder="add meal..."
+                  />
+                </div>
+                dinner
+                <div key={0}>
+                  <input
+                    className="input"
+                    type="text"
+                    value={inputValues[20]}
+                    onChange={(e) => handleInputChange(e, 20)}
+                    placeholder="add meal..."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   );
 }
 
 export default MealPlan;
+
