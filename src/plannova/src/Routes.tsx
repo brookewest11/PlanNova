518c5abb7b9d4ca8653d9b4a8a4329a592e14b63
Nathan-Mignot <nate.mignot@gmail.com>
2024-02-25 16:46:24 -0600

    re-enabled the login page

--- src/plannova/src/Routes.tsx
+++ src/plannova/src/Routes.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Home from './modules/Home'; 
-import MealPlan from './modules/MealPlan';
-import List from './modules/List';
-import FitnessTracker from './modules/FitnessTracker';
-import Login from './modules/Login';
-import NewUser from './modules/NewUser';
+import Home from "./modules/Home";
+import MealPlan from "./modules/MealPlan";
+import List from "./modules/List";
+import FitnessTracker from "./modules/FitnessTracker";
+import Login from "./modules/Login";
+import NewUser from "./modules/NewUser";
 
 function AppRoutes() {
-return (
+  return (
     <Routes>
-         <Route path="/" element={<Home />} /> 
-        <Route path="/home" element={<Home />} /> 
-        <Route path="/meal-planning" element={<MealPlan />} /> 
-        <Route path="/list" element={<List />} /> 
-        <Route path="/fitness-tracker" element={<FitnessTracker />} /> 
-        <Route path="/new-user" element={<NewUser />} /> 
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/meal-planning" element={<MealPlan />} />
+      <Route path="/list" element={<List />} />
+      <Route path="/fitness-tracker" element={<FitnessTracker />} />
+      <Route path="/new-user" element={<NewUser />} />
     </Routes>
-    );
+  );
 }
 
 export default AppRoutes;
